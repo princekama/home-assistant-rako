faca888e895b8c5355e001fa0bcbff3e44210ba5
Prince Kamalanathan <33226588+princekama@users.noreply.github.com>
2024-10-25 10:49:51 +0100

    Initial implementation (#2)

---
+++ custom_components/rako/config_flow.py
@@ -0,0 +1,73 @@
+"""Config flow for Rako integration."""
+
+from __future__ import annotations
+
+import asyncio
+import logging
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.const import CONF_HOST, CONF_NAME
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
+from rakopy.hub import Hub
+from .const import DOMAIN, TIMEOUT
+
+_LOGGER = logging.getLogger(__name__)
+
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME): str,
+        vol.Required(CONF_HOST): str
+    }
+)
+
+
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+    hub = Hub(data[CONF_NAME], data[CONF_HOST])
+    try:
+        hub_info = await asyncio.wait_for(
+            hub.get_hub_status(), timeout=TIMEOUT
+        )
+    except:
+        raise CannotConnect
+
+    return {"title": "Rako Hub", "hub_id": hub_info.id}
+
+
+class ConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Rako."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle the initial step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            try:
+                info = await validate_input(self.hass, user_input)
+                await self.async_set_unique_id(f"Rako_Hub_{info["hub_id"]}")
+                self._abort_if_unique_id_configured()
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except Exception:
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(title=info["title"], data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
---
+++ custom_components/rako/const.py
@@ -0,0 +1,4 @@
+"""Constants for the Rako integration."""
+
+DOMAIN = "rako"
+TIMEOUT = 3
